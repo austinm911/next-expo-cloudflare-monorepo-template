fada3a53e837e35ab42a12e56f1525e013a3fc55
Austin <31991302+austinm911@users.noreply.github.com>
2024-04-25 11:21:28 -0700

    add native eslint

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,6 +1,20 @@
-import "../global.css";
-import { Slot } from "expo-router";
+import '../global.css'
+
+import { Slot } from 'expo-router'
+import { useReactQueryDevTools } from '@dev-plugins/react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function Layout() {
-  return <Slot />;
+    return <Providers />
+}
+
+const queryClient = new QueryClient()
+
+function Providers() {
+    useReactQueryDevTools(queryClient)
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Slot />
+        </QueryClientProvider>
+    )
 }
--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -17,11 +17,11 @@ function Content() {
     return (
         <View className='flex-1'>
             <View className='py-12 md:py-24 lg:py-32 xl:py-48'>
-                <View className='px-4 bg-green-200 md:px-6'>
-                    <View className='flex flex-col gap-4 items-center text-center'>
+                <View className='bg-green-200 px-4 md:px-6'>
+                    <View className='flex flex-col items-center gap-4 text-center'>
                         <Text
                             role='heading'
-                            className='text-3xl font-bold tracking-tighter text-center text-cyan-300 bg-red-900 native:text-5xl sm:text-4xl md:text-5xl lg:text-6xl'
+                            className='native:text-5xl bg-red-900 text-center text-3xl font-bold tracking-tighter text-cyan-300 sm:text-4xl md:text-5xl lg:text-6xl'
                         >
                             Welcome to Project ACME
                         </Text>
@@ -32,7 +32,7 @@ function Content() {
                         <View className='gap-4'>
                             <Link
                                 suppressHighlighting
-                                className='flex overflow-hidden justify-center items-center px-4 py-2 h-9 text-sm font-medium text-gray-50 bg-gray-900 rounded-md transition-colors web:shadow ios:shadow web:focus-visible:outline-none web:focus-visible:ring-1 hover:bg-gray-900/90 focus-visible:ring-gray-950 active:bg-gray-400/90 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300'
+                                className='web:shadow ios:shadow web:focus-visible:outline-none web:focus-visible:ring-1 flex h-9 items-center justify-center overflow-hidden rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-gray-50 transition-colors hover:bg-gray-900/90 focus-visible:ring-gray-950 active:bg-gray-400/90 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300'
                                 href='/'
                             >
                                 Explore
@@ -49,18 +49,18 @@ function Header() {
     const { top } = useSafeAreaInsets()
     return (
         <View style={{ paddingTop: top }}>
-            <View className='flex flex-row justify-between items-center px-4 h-14 lg:px-6'>
-                <Link className='flex-1 justify-center items-center font-bold' href='/'>
+            <View className='flex h-14 flex-row items-center justify-between px-4 lg:px-6'>
+                <Link className='flex-1 items-center justify-center font-bold' href='/'>
                     ACME
                 </Link>
                 <View className='flex flex-row gap-4 sm:gap-6'>
-                    <Link className='font-medium text-md web:underline-offset-4 hover:underline' href='/'>
+                    <Link className='text-md web:underline-offset-4 font-medium hover:underline' href='/'>
                         About
                     </Link>
-                    <Link className='font-medium text-md web:underline-offset-4 hover:underline' href='/'>
+                    <Link className='text-md web:underline-offset-4 font-medium hover:underline' href='/'>
                         Product
                     </Link>
-                    <Link className='font-medium text-md web:underline-offset-4 hover:underline' href='/'>
+                    <Link className='text-md web:underline-offset-4 font-medium hover:underline' href='/'>
                         Pricing
                     </Link>
                 </View>
@@ -72,7 +72,7 @@ function Header() {
 function Footer() {
     const { bottom } = useSafeAreaInsets()
     return (
-        <View className='flex bg-gray-100 native:hidden shrink-0' style={{ paddingBottom: bottom }}>
+        <View className='native:hidden flex shrink-0 bg-gray-100' style={{ paddingBottom: bottom }}>
             <View className='flex-1 items-start px-4 py-6 md:px-6'>
                 <Text className={'text-center text-gray-700'}>© {new Date().getFullYear()} Me</Text>
             </View>
