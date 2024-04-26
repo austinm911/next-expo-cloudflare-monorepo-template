5aa6ddee7f71e3f2066ef94d90b8b110de81130a
Austin <31991302+austinm911@users.noreply.github.com>
2024-04-26 10:10:21 -0700

    This reverts a file in commit f3ffb1339a201c23cddb0be2d6f7657a73bfde22.

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -22,5 +22,5 @@ export default app
 
 type AppType = typeof routes
 
-const siteUrl = 'https://my-app.austinm911.workers.dev/'
-export const client = hc<AppType>(siteUrl)
+const apiUrl = 'https://localhost:8787'
+export const client = hc<AppType>(apiUrl)
